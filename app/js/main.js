70220f0020a2920af7897cafc2d2519d68743d9d
Dishones2 <andrej13524@gmail.com>
2020-07-10 18:01:57 +0600

    added products-page

--- app/js/main.js
+++ app/js/main.js
@@ -12,11 +12,18 @@ $(function(){
         rating: 5,
         starWidth: "12px",
         readOnly: true
-      });
+    });
 
+    
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+    });
+    
     var mixer = mixitup('.products__inner-box');
-
-
-
     
 });
